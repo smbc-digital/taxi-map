fedb953aa2b3a21205e3dbd293948633ec30683b
Daniel Kinder <daniel.kinder@stockport.gov.uk>
2021-01-21 11:47:57 +0000

    Popup change

--- src/Popups/index.js
+++ src/Popups/index.js
@@ -2,7 +2,7 @@ const taxirankspointsPopup = (feature, layer) => {
   
   const content = `<div class="item"><i class="fa fa-taxi" aria-hidden="true"></i><p class="title">Taxi Ranks</p>
   <p></p>
-  <p class="info">Name: ${feature.properties.desc}</p>
+  <p class="info">Location: ${feature.properties.desc}</p>
   
   </div>`
  
