1f9fdff7f997051467948e8653420b0436b576ab
Daniel Kinder <daniel.kinder@stockport.gov.uk>
2021-01-21 16:12:53 +0000

    default zoom edit

--- src/Configuration.js
+++ src/Configuration.js
@@ -4,8 +4,8 @@ import { taxirankspointsStyle, taxiranksStyle } from './Styles'
 
 const Configuration = {
     Map: {
-        StartingLatLng: [53.3915, -2.125143],
-        StartingZoom: 12,
+        StartingLatLng: [53.408841 , -2.1587385],
+        StartingZoom: 8,
         FullscreenControl: true,
         DisplayLayerControls: true,
         DisplayGrayScale: true,
